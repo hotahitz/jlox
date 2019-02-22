2991878b5dfaa95a9788483ae47948ae0ea387e5
hotahitz <hitesh.hota2013@vit.ac.in>
2019-02-22 20:56:32 +0530

    parser now accepts statements

--- com/hotahitz/lox/Interpreter.java
+++ com/hotahitz/lox/Interpreter.java
@@ -1,6 +1,8 @@
 package com.hotahitz.lox;
 
-class Interpreter implements Expr.Visitor<Object>{
+import java.util.List;
+
+class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
   @Override
   public Object visitLiteralExpr(Expr.Literal expr){
     return expr.value;
@@ -67,6 +69,23 @@ class Interpreter implements Expr.Visitor<Object>{
     return expr.accept(this);
   }
 
+  private void execute(Stmt stmt){
+    stmt.accept(this);
+  }
+
+  @Override
+  public Void visitExpressionStmt(Stmt.Expression stmt){
+    evaluate(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitPrintStmt(Stmt.Print stmt){
+    Object value = evaluate(stmt.expression);
+    System.out.println(stringify(value));
+    return null;
+  }
+
   @Override
   public Object visitBinaryExpr(Expr.Binary expr){
     Object left = evaluate(expr.left);
@@ -112,11 +131,12 @@ class Interpreter implements Expr.Visitor<Object>{
     return null;
   }
 
-  void interpret(Expr expression){
-    try {
-      Object value = evaluate(expression);
-      System.out.println(stringify(value));
-    } catch(RuntimeError error) {
+  void interpret(List<Stmt> statements){
+    try{
+      for(Stmt statement:statements){
+        execute(statement);
+      }
+    } catch (RuntimeError error){
       Lox.runtimeError(error);
     }
   }
