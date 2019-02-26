ef41c4ef5f99de2a7535ad626b69df66b4eba829
hotahitz <hitesh.hota2013@vit.ac.in>
2019-02-26 20:21:02 +0530

    Chapter - 8 complete, now the interpreter remembers scope

--- com/hotahitz/lox/Interpreter.java
+++ com/hotahitz/lox/Interpreter.java
@@ -3,6 +3,18 @@ package com.hotahitz.lox;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
+  private Environment environment = new Environment();
+
+  void interpret(List<Stmt> statements){
+    try{
+      for(Stmt statement:statements){
+        execute(statement);
+      }
+    } catch (RuntimeError error){
+      Lox.runtimeError(error);
+    }
+  }
+
   @Override
   public Object visitLiteralExpr(Expr.Literal expr){
     return expr.value;
@@ -21,6 +33,11 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
     }
 
     return null;
+  }
+
+  @Override
+  public Object visitVariableExpr(Expr.Variable expr){
+    return environment.get(expr.name);
   }
 
   private void checkNumberOperand(Token operator, Object operand){
@@ -73,6 +90,24 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
     stmt.accept(this);
   }
 
+  void executeBlock(List<Stmt> statements, Environment environment){
+    Environment previous = this.environment;
+    try{
+      this.environment = environment;
+      for(Stmt statement: statements){
+        execute(statement);
+      }
+    }finally{
+      this.environment = previous;
+    }
+  }
+
+  @Override
+  public Void visitBlockStmt(Stmt.Block stmt){
+    executeBlock(stmt.statements, new Environment(environment));
+    return null;
+  }
+
   @Override
   public Void visitExpressionStmt(Stmt.Expression stmt){
     evaluate(stmt.expression);
@@ -84,6 +119,25 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
     Object value = evaluate(stmt.expression);
     System.out.println(stringify(value));
     return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt){
+    Object value = null;
+    if(stmt.initializer != null){
+      value = evaluate(stmt.initializer);
+    }
+
+    environment.define(stmt.name.lexeme, value);
+    return null;
+  }
+
+  @Override
+  public Object visitAssignExpr(Expr.Assign expr){
+    Object value = evaluate(expr.value);
+
+    environment.assign(expr.name, value);
+    return value;
   }
 
   @Override
@@ -129,15 +183,5 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
         return (double)left * (double)right;
     }
     return null;
-  }
-
-  void interpret(List<Stmt> statements){
-    try{
-      for(Stmt statement:statements){
-        execute(statement);
-      }
-    } catch (RuntimeError error){
-      Lox.runtimeError(error);
-    }
   }
 }
