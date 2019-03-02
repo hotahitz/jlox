51f88841e936a34862762d966e5b173be9cae33f
hotahitz <hitesh.hota2013@vit.ac.in>
2019-03-02 19:21:18 +0530

    Chapter complete. functions added

--- com/hotahitz/lox/Parser.java
+++ com/hotahitz/lox/Parser.java
@@ -43,6 +43,7 @@ class Parser{
     if(match(FOR)) return forStatement();
     if(match(IF)) return ifStatement();
     if(match(PRINT)) return printStatement();
+    if(match(RETURN)) return returnStatement();
     if(match(WHILE)) return whileStatement();
     if(match(LEFT_BRACE)) return new Stmt.Block(block());
 
@@ -111,6 +112,17 @@ class Parser{
     return new Stmt.Print(value);
   }
 
+  private Stmt returnStatement(){
+    Token keyword = previous();
+    Expr value = null;
+    if(!check(SEMICOLON)){
+      value = expression();
+    }
+
+    consume(SEMICOLON, "Expect ';' after return value.");
+    return new Stmt.Return(keyword, value);
+  }
+
   private Stmt varDeclaration(){
     Token name = consume(IDENTIFIER, "Expect variable name.");
 
@@ -140,7 +152,7 @@ class Parser{
 
   private Stmt.Function function(String kind){
     Token name = consume(IDENTIFIER, "Expect "+kind+" name.");
-    consume(LEFT_PAREN, "Expect '(' after "+kind+" name".);
+    consume(LEFT_PAREN, "Expect '(' after "+kind+" name.");
     List<Token> parameters = new ArrayList<>();
     if(!check(RIGHT_PAREN)){
       do{
