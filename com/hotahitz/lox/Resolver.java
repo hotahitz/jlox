293b0fdb3e983e4022e8264281d2eba9d7456dbc
hotahitz <hitesh.hota2013@vit.ac.in>
2019-03-07 12:07:45 +0530

    Inheritance added

--- com/hotahitz/lox/Resolver.java
+++ com/hotahitz/lox/Resolver.java
@@ -29,7 +29,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
 
   private enum ClassType{
     NONE,
-    CLASS
+    CLASS,
+    SUBCLASS
   }
 
   private ClassType currentClass = ClassType.NONE;
@@ -49,7 +50,17 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
 
     declare(stmt.name);
 
+    if(stmt.superclass != null){
+      currentClass = ClassType.SUBCLASS;
+      resolve(stmt.superclass);
+    }
+
     define(stmt.name);
+
+    if(stmt.superclass != null){
+      beginScope();
+      scopes.peek().put("super",true);
+    }
 
     beginScope();
     scopes.peek().put("this", true);
@@ -63,6 +74,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
     }
 
     endScope();
+
+    if(stmt.superclass != null) endScope();
 
     currentClass = enclosingClass;
 
@@ -184,6 +197,19 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
   public Void visitSetExpr(Expr.Set expr){
     resolve(expr.value);
     resolve(expr.object);
+    return null;
+  }
+
+  @Override
+  public Void visitSuperExpr(Expr.Super expr){
+    if(currentClass == ClassType.NONE){
+      Lox.error(expr.keyword,
+          "Cannot use 'super' outside of a class.");
+    } else if(currentClass != ClassType.SUBCLASS){
+      Lox.error(expr.keyword,
+          "Cannot use 'super' in a class with no superclass.");
+    }
+    resolveLocal(expr, expr.keyword);
     return null;
   }
 
